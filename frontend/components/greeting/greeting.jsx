e3e0a31f4e0c45bc2d5aec531a96715d7bf7a1b4
Brendan <brendanekane@gmail.com>
2018-03-09 01:12:31 -0500

    styled greeting better

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -17,8 +17,9 @@ class Greeting extends React.Component {
         <div className='greeting-container'>
           <img className='greeting-user-picture' src='https://cdn.business2community.com/wp-content/uploads/2013/06/Michael-Scott.png' alt='profile picture'></img>
           <div className='greeting-user-info'>
-            <h2 className='greeting-user-name'>{this.props.currentUser.email.substring(0, this.props.currentUser.email.lastIndexOf("@"))}</h2>
-            <p className='greeting-transactions-num'>({this.props.currentUser.transactions})</p>
+            <h2 className='greeting-user-name'>
+              {this.props.currentUser.email.substring(0, this.props.currentUser.email.lastIndexOf("@"))} ({(this.props.currentUser.transactions)})
+            </h2>
             <p className='greeting-transactions-full'>{this.props.currentUser.transactions} Transactions</p>
             <br/>
           </div>
