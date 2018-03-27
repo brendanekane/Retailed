678cffe968c58f2b8a55d6da740e5be3ed07a281
Brendan <brendanekane@gmail.com>
2018-03-27 13:49:53 -0400

    removed logger and minified code warning in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
 var path = require('path');
 
+
+let plugins = [];
+
+const devPlugins = [];
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
 module.exports = {
   entry: './frontend/retailed.jsx',
   output: {
     path: path.resolve(__dirname, 'app/assets/javascripts'),
     filename: './bundle.js',
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
