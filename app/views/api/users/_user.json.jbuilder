ef4f401edd20b637a7e6dd03a61440ba62e8d960
Brendan <brendanekane@gmail.com>
2018-03-29 13:38:39 -0400

    fixed 500 error on session modal

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -8,8 +8,6 @@ url = asset_path(user.avatar.url)
 if url.include? "assets"
   url
 else
-  debugger
   url[3] = url[3] + 's'
-  debugger
 end
 json.image_url asset_path(url)
--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -11,11 +11,6 @@ class SessionForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    this.props.getUsers();
-    this.props.getProducts();
-  }
-
   update(field) {
     return e => this.setState({
       [field]: e.currentTarget.value
