7ee33f8626cf372b1d6a390629e7765281d19da3
Brendan <brendanekane@gmail.com>
2018-03-29 15:31:10 -0400

    testing aws secure image serve

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :products
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "default_avatar.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/, s3_protocol: :https
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   has_many :cart_items#, dependent: destroy
   has_many :cart_products,
--- app/views/api/product_photos/_product_photo.json.jbuilder
+++ app/views/api/product_photos/_product_photo.json.jbuilder
@@ -1,4 +1,2 @@
 json.product_id product.id
-url = asset_path(product_photo.image.url)
-url[3] = url[3] + 's'
-json.image_url asset_path(url)
+json.image_url asset_path(product_photo.image.url)
