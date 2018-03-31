acf136ed3063f32ef86049023ad6dcd1a85ba572
Brendan <brendanekane@gmail.com>
2018-03-31 12:45:37 -0400

    removed unnecessary ajax request on product page

--- frontend/components/product/product.jsx
+++ frontend/components/product/product.jsx
@@ -12,7 +12,7 @@ class Product extends React.Component {
   componentDidMount() {
     this.props.getProduct(this.props.match.params.productId).then( () => {
       this.props.getOneUser(this.props.product.user_id);
-      this.props.getItems();
+      // this.props.getItems();
     });
   }
 
