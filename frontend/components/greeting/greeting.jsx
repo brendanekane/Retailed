09eb7acc1679afead47930333846844e0fb417c7
Brendan <brendanekane@gmail.com>
2018-04-02 17:47:47 -0400

    greeting styling

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -15,18 +15,20 @@ class Greeting extends React.Component {
       return (
         <div className='greeting-container'>
           <img className='greeting-user-picture' src={this.props.currentUser.image_url} alt='profile picture'></img>
-          <div className='greeting-user-info'>
-            <h2 className='greeting-user-name'>
-              {this.props.currentUserName} ({this.props.transactions})
-            </h2>
-            <div className='greeting-sub-info'>
-              <p className='greeting-transactions-full'>{this.props.transactions} Transactions</p>
-              <p className='greeting-feedback'>No feedback yet</p>
-              <p className='greeting-location'>{this.props.location}</p>
-              <p className='greeting-dimensions'>{this.props.height}''  {this.props.weight} lbs</p>
+          <div className='greeting-user-info-container'>
+            <div className='greeting-user-info'>
+              <h2 className='greeting-user-name'>
+                {this.props.currentUserName} ({this.props.transactions})
+              </h2>
+              <div className='greeting-sub-info'>
+                <p className='greeting-transactions-full'>{this.props.transactions} Transactions</p>
+                <p className='greeting-feedback'>No feedback yet</p>
+                <p className='greeting-location'>{this.props.location}</p>
+                <p className='greeting-dimensions'>{this.props.height}''  {this.props.weight} lbs</p>
+              </div>
             </div>
+            <Link to="/users/settings" className='greeting-edit-link'>EDIT </Link>
           </div>
-          <Link to="/users/settings" className='greeting-edit-link'>EDIT </Link>
         </div>
       );
     } else {
