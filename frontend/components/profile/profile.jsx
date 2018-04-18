fb7f05ce519105619f312bb54da9d42651f9ccb7
Brendan <brendanekane@gmail.com>
2018-04-18 17:26:43 -0400

    refactor, made presentational component

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter, Route } from 'react-router-dom';
-import ProfileIndexContainer from 'components/profile/profile_index_container';
+import ProfileIndex from 'components/profile/profile_index';
 import ProfileEditFormContainer from 'components/profile/profile_edit_form_container';
 import ProfileItemsContainer from 'components/profile/profile_items_container';
 import ProfileMessagesContainer from 'components/profile/profile_messages_container';
@@ -9,7 +9,7 @@ const Profile = () => {
   return (
     <div className="profile-container">
       <div className="profile-index-container">
-        <ProfileIndexContainer />
+        <ProfileIndex />
         <div>
           <Route path='/users/settings' component={ ProfileEditFormContainer } />
           <Route path='/users/myitems' component={ ProfileItemsContainer } />
